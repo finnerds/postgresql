e29d63fd8497517d0a22a1067f3d8467a9e20890
Peter (dkdndes) <dkdndes@gmail.com>
2019-07-15 14:42:02 +0200

    typo

--- slave/create_extension.sh
+++ slave/create_extension.sh
@@ -1,4 +1,3 @@
-
 # Because both template1 and the user postgres database have already been created,
 # we need to create the hstore extension in template1 and then recreate the postgres database.
 #
